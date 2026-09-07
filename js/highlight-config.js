dde7c9ef1bdf395e5e069b4708dd7b11aad68ced
Morteux <45830923+Morteux@users.noreply.github.com>
2026-09-07 18:11:33 +0900

    Commit final pre content

--- js/highlight-config.js
+++ js/highlight-config.js
@@ -14,17 +14,96 @@
  * Ejemplo rápido: descomenta y ajusta las líneas de abajo.
  */
 export const highlightWords = {
-  "corbeta": "#d3d3d3",
-  "corbetas": "#d3d3d3",
-  "destructor": "#55ff37",
-  "destructores": "#55ff37",
-  "crucero": "#375cff",
-  "cruceros": "#375cff",
-  "acorazado": "#ca29ff",
-  "acorazados": "#ca29ff",
-  "titán": "#fff42b",
-  "titanes": "#fff42b",
-  
-  "aura": "#ff2c2c",
-  "auras": "#ff2c2c"
+
+    ////////////////////////////////////////////////////////// Español
+    "corbeta": "#d3d3d3",
+    "corbetas": "#d3d3d3",
+    "destructor": "#55ff37",
+    "destructores": "#55ff37",
+    "crucero": "#375cff",
+    "cruceros": "#375cff",
+    "acorazado": "#ca29ff",
+    "acorazados": "#ca29ff",
+    "titán": "#fff42b",
+    "titanes": "#fff42b",
+
+    "inesperados": "#332cff",
+    "vehements": "#fbff2c",
+    "aberrantes": "#2cff30",
+    "prethoryn": "#ffb92c",
+    "contingencia": "#ff0000",
+    "cetana": "#fff42c",
+    "reina sintetica": "#fff42c",
+
+    "cinetica": "#ffae2c",
+    "cineticas": "#ffae2c",
+    "energia": "#2c33ff",
+    "energias": "#2c33ff",
+
+    "casco": "#ffae2c",
+    "cascos": "#ffae2c",
+    "escudo": "#2c33ff",
+    "escudos": "#2c33ff",
+
+    "materia oscura": "#8900af",
+    "psionico": "#8900af",
+    "psionicos": "#8900af",
+
+    ////////////////////////////////////////////////////////// English
+    "corvette": "#d3d3d3",
+    "corvettes": "#d3d3d3",
+    "destroyer": "#55ff37",
+    "destroyers": "#55ff37",
+    "cruiser": "#375cff",
+    "cruisers": "#375cff",
+    "battleship": "#ca29ff",
+    "battleships": "#ca29ff",
+    "titan": "#fff42b",
+    "titans": "#fff42b",
+
+    "unbidden": "#332cff",
+    "vehement": "#fbff2c",
+    "aberrant": "#2cff30",
+    "prethoryn": "#ffb92c",
+    "contingency": "#ff0000",
+    "cetana": "#fff42c",
+    "synthetic queen": "#fff42c",
+
+    "kinetic": "#ffae2c",
+    "kinetics": "#ffae2c",
+    "energy": "#2c33ff",
+    "energies": "#2c33ff",
+
+    "hull": "#ffae2c",
+    "hulls": "#ffae2c",
+    "shield": "#2c33ff",
+    "shields": "#2c33ff",
+
+    "dark matter": "#8900af",
+    "psionic": "#8900af",
+    "psionics": "#8900af",
+
+    ////////////////////////////////////////////////////////// 日本語
+    "コルベット": "#d3d3d3",
+    "駆逐艦": "#55ff37",
+    "巡洋艦": "#375cff",
+    "戦艦": "#ca29ff",
+    "タイタン": "#fff42b",
+
+    "予想外": "#332cff",
+    "ヴェーメント": "#fbff2c",
+    "アベラント": "#2cff30",
+    "プレスリン": "#ffb92c",
+    "コンティンジェンシー": "#ff0000",
+    "セタナ": "#fff42c",
+    "合成の女王": "#fff42c",
+
+    "運動": "#ffae2c",
+    "エネルギー": "#2c33ff",
+
+    "船体": "#ffae2c",
+    "シールド": "#2c33ff",
+
+    "ダークマター": "#8900af",
+    "サイオニック": "#8900af"
 };
